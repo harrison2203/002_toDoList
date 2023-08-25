d97ec120b6e2fddfb56c8e131f73022b00dd4ae0
Harrison <harrison.alzate-montes@epitech.eu>
2023-08-25 19:21:39 +0200

    [ADD]firt commit

---
+++ vue-todos/src/components/TodoButtonComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+    <button @click="createTodo">
+    <slot>
+      Create
+    </slot>
+    </button>
+
+</div>
+
+</template>
+
+<style lang="scss" scoped>
+  button {
+    padding: 8px 16px;
+    border: none;
+  }
+
+  </style>
+
---
+++ vue-todos/src/views/TodosView.vue
@@ -0,0 +1,44 @@
+<script setup>
+import {ref} from "vue";
+import {uid} from 'uid'; // unique identifier. chaque élément aura un id unique
+import TodoCreatorComponent from '../components/TodoCreatorComponent.vue';
+
+const todoList = ref([]);
+
+const createTodo = (todo) => {
+  todoList.value.push({
+    id: uid(),
+    todo,
+    isCompleted: null,
+    isEditing: null
+  })
+}
+
+
+
+</script>
+
+<template>
+  <main>
+    <h1>Create Todo</h1>
+    <TodoCreatorComponent @create-todo="createTodo"/>
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}
+
+
+</style>
